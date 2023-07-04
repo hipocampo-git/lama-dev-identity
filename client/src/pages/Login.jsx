a7e5cb59372e9e0752839185dc8a5e56a75b9754
Oliver Pike <oliver@hipocampo.com>
2023-07-03 19:44:21 -0700

    changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,50 @@
 import Facebook from "../img/facebook.png";
 import Github from "../img/github.png";
+import { useEffect, useRef } from "react";
 
 const GOOGLE_CLIENT_ID =
     "561326950243-dm3ee9l77codsuq9c5g4ulaq78q01pdc.apps.googleusercontent.com";
 
 const Login = () => {
+
+  const googleButton = useRef(null);
+
+  const loadScript = (src) =>
+      new Promise((resolve, reject) => {
+        if (document.querySelector(`script[src="${src}"]`)) return resolve()
+        const script = document.createElement('script')
+        script.src = src
+        script.onload = () => resolve()
+        script.onerror = (err) => reject(err)
+        document.body.appendChild(script)
+      })
+
+  useEffect(() => {
+    const src = 'https://accounts.google.com/gsi/client'
+
+    loadScript(src)
+        .then(() => {
+          /* global google */
+          console.log(google);
+          debugger;
+          google.accounts.id.initialize({
+            client_id: GOOGLE_CLIENT_ID,
+            login_url: "http://localhost:5000/auth/google",
+            auto_prompt: false
+            // callback: handleCredentialResponse,
+          })
+          google.accounts.id.renderButton(
+              googleButton.current, //this is a ref hook to the div in the official example
+              { theme: 'outline', size: 'large' } // customization attributes
+          )
+        })
+        .catch(console.error)
+
+    return () => {
+      const scriptTag = document.querySelector(`script[src="${src}"]`)
+      if (scriptTag) document.body.removeChild(scriptTag)
+    }
+  }, [])
 
   const github = () => {
     window.open("http://localhost:5000/auth/github", "_self");
@@ -19,7 +59,8 @@ const Login = () => {
       <h1 className="loginTitle">Choose a Login Method</h1>
       <div className="wrapper">
         <div className="left">
-          <div id="g_id_onload"
+          <div ref={googleButton}></div>
+{/*          <div id="g_id_onload"
                data-client_id={GOOGLE_CLIENT_ID}
                data-login_uri="http://localhost:5000/auth/google"
                data-auto_prompt="true">
@@ -31,7 +72,7 @@ const Login = () => {
                data-text="sign_in_with"
                data-shape="rectangular"
                data-logo_alignment="left">
-          </div>
+          </div>*/}
 
           <div className="loginButton facebook" onClick={facebook}>
             <img src={Facebook} alt="" className="icon" />
